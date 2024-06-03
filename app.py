5d1eb09f29cf8986dd7f344bcf434a272c4e8676
Akash A.S <149227673+a-s-akash@users.noreply.github.com>
2024-06-03 11:35:33 +0530

    Update app.py

--- app.py
+++ app.py
@@ -3,5 +3,5 @@ import streamlit as st
 st.set_page_config(page_title="aagayaa",page_icon=":tada:",layout="wide")
 
 st.subheader("welcome")
-st.title("just for fu")
+st.title("just for fun")
 st.write("for marks")
